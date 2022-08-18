9f934f404fc1561f45b4b10d343f2f6ddab6da26
tim <timawbi@gmail.com>
2022-08-18 18:44:01 +0900

    update

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ let txt = document.querySelectorAll('[data-scramble-text]')
 
 function sA(x) {let array = x; for (let i = array.length - 1; i > 0; i--) {let j = Math.floor(Math.random() * (i + 1));[array[i], array[j]] = [array[j], array[i]];}return array;}
 
-txt.forEach(t => {
-    
+txt.forEach(t => {    
     let original = t.textContent.split("")
     let random = t.textContent.split("") 
     t.textContent=''
-
     let origin = document.createElement('span')
     let container = document.createElement('span')
- 
     let jumble = sA(random)
-    
     let c = 0
-
     t.removeAttribute('style')
 
     t.appendChild(origin)
