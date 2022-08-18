4b3b62b4c7c0268bead138ea37fbaa4694fda872
tim <timawbi@gmail.com>
2022-08-18 18:40:30 +0900

    Update

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ let txt = document.querySelectorAll('[data-scramble-text]')
 function sA(x) {let array = x; for (let i = array.length - 1; i > 0; i--) {let j = Math.floor(Math.random() * (i + 1));[array[i], array[j]] = [array[j], array[i]];}return array;}
 
 txt.forEach(t => {
+    
     let original = t.textContent.split("")
     let random = t.textContent.split("") 
-    t.tContent=''
+    t.textContent=''
+
     let origin = document.createElement('span')
     let container = document.createElement('span')
  
@@ -13,24 +15,30 @@ txt.forEach(t => {
     
     let c = 0
 
+    t.removeAttribute('style')
+
     t.appendChild(origin)
     t.appendChild(container)
 
-    let looper =  setInterval(() => {
-        origin.textContent += original[c]
 
-        jumble.pop()
-        sA(jumble)
-        container.textContent = jumble.join("")
-        c++;
-
-        if(c > original.length-1){
-          
-            clearInterval(looper)
-
-        }
-    }, 50 );
+    window.onload = (event) => {
+        let looper =  setInterval(() => {
+            origin.textContent += original[c]
     
+            jumble.pop()
+            sA(jumble)
+            container.textContent = jumble.join("")
+            c++;
+    
+            if(c > original.length-1){
+                clearInterval(looper)
+            }
+        }, 50 );
+        
+
+    };
+
+
 
 
     
